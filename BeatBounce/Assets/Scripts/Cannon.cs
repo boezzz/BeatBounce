58540058efaaaebe851bf9f8cdf49e6afa0a9533
Jaclyn Chen <jaclyn.chen5@gmail.com>
2024-02-27 18:42:17 -0800

    imported cannon asset, created script for cannon launch

---
+++ BeatBounce/Assets/Scripts/Cannon.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    //[SerializeField] private Transform _turret;
+    //[SerializeField] private GameObject _firingPosition;
+    //[SerializeField] private GameObject _cannonBall;
+
+    public Transform _turret;
+    public GameObject _firingPosition;
+
+    public GameObject _cannonBall;
+
+    public void Fire() => Instantiate(_cannonBall, _firingPosition.transform.position, _firingPosition.transform.rotation);
+    public void MoveUp() => _turret.transform.Rotate(Vector3.right, 1);
+    public void DownUp() => _turret.transform.Rotate(Vector3.right, -1);
+    public void RightUp() => _turret.transform.Rotate(Vector3.up, -1);
+    public void LeftUp() => _turret.transform.Rotate(Vector3.up, 1);
+
+}
