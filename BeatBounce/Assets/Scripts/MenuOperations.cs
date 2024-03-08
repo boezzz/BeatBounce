57ccb27663a001c5019c4cd62fed7fadbaaa944e
Jaclyn Chen <jaclyn.chen5@gmail.com>
2024-03-07 23:24:36 -0800

    added prefabs, made button that instantiates a cannonball with 0 velocity

--- BeatBounce/Assets/Scripts/MenuOperations.cs
+++ BeatBounce/Assets/Scripts/MenuOperations.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class MenuOperations : MonoBehaviour
 {
+
+
+    // for ball launcher
+    //public Transform _turret;
+    //public Transform _barrel;
+    public GameObject _firingPosition;
+
+    public GameObject _cannonBall;
+
+
     // this refer to the hand menu, add in world operations
     public void OnClearButtonClicked() {
         // clear all panels in space
@@ -11,4 +21,30 @@ public class MenuOperations : MonoBehaviour
             Destroy(each_panel);
         }
     }
+
+
+
+
+    // Launch settings functions
+    //public void makeCannonBall()
+    //{
+
+    //    GameObject newCannonBall = Instantiate(_cannonBall, _firingPosition.transform.position, _firingPosition.transform.rotation);
+    //    CannonBall cannonBallScript = newCannonBall.GetComponent<CannonBall>();
+    //    if (cannonBallScript != null)
+    //    {
+    //        //cannonBallScript.SetVelocity(mySlider.value);
+    //        cannonBallScript.SetVelocity(700f);
+
+    //    }
+
+    //}
+
+
+    public void spawnBall()
+    {
+        Instantiate(_cannonBall, _firingPosition.transform.position, _firingPosition.transform.rotation);
+    }
+
+
 }
